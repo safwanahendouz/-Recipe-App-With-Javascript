1b553a8b9c5e892fdf589d0fc8cac554ebad972c
safwanahendouz <safwanahendouz241@gmail.com>
2026-03-27 16:43:56 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Initial References
+let result = document.getElementById("result");
+let searchbtn = document.getElementById("search-btn");
+let url = "https://www.themealdb.com/api/json/v1/1/search-php?s=";
