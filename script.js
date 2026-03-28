ed22aa2c158f55313a0a155f38dc8aab4e950a24
safwanahendouz <safwanahendouz241@gmail.com>
2026-03-28 15:54:03 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,27 @@
 let result = document.getElementById("result");
 let searchbtn = document.getElementById("search-btn");
 let url = "https://www.themealdb.com/api/json/v1/1/search-php?s=";
+
+let userInp = document.getElementById("user-inp").value;
+
+fetch(url + "big mac") 
+    .then((response) => response.json())
+    .then((data) => {
+        let myMeal = data.meals[0];
+        console.log(myMeal);
+        console.log(myMeal.strMealThumb);
+        console.log(myMeal.strMeal);
+        console.log(myMeal.strArea);
+        console.log(myMeal.strInsructions);
+        let count = 1;
+        let ingredients = [];
+        for(let i in myMeal)  {
+            let ingredient = "";
+            let measure = "";
+            if (i.startsWith("strIngredient") && myMeal[i]) {
+                ingredient = myMeal[i];
+                measure = "";
+                
+            }
+        }
+    })
